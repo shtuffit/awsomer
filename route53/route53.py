2f5e38e3effaca510d4ad5127f35b58cc7675983
Chris Montanaro <monty@shtuff.it>
2014-03-05 17:01:34 -0600

    Removed text records from Zone cloner

--- route53/route53.py
+++ route53/route53.py
@@ -9,7 +9,9 @@ def zone_clone(source, dest):
     conn = connector()
     changes = ResourceRecordSets(conn, dest.id)
     for record in source.get_records():
-      if record.type != 'NS' and record.type != 'SOA':
+      if record.type == 'TXT':
+        continue
+      elif record.type != 'NS' and record.type != 'SOA':
         new = record.name.replace(source.name, dest.name)
         if record.alias_dns_name:
           change = changes.add_change("CREATE", new, record.type, record.ttl, alias_dns_name=record.alias_dns_name, alias_hosted_zone_id=record.alias_hosted_zone_id, alias_evaluate_target_health=False)
